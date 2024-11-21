4f4acfa2d4f2804122d4293af55bf58cc7cb57d0
Vadim Ivlev <vadimi.vlev@gmail.com>
2024-11-21 11:03:35 +0000

    .

--- bash/say.sh
+++ bash/say.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
 #https://serverfault.com/questions/72476/clean-way-to-write-complex-multi-line-string-to-a-variable
-#
-#IFS='' read -r -d '' String <<"EOF"
-#<?xml version="1.0" encoding='UTF-8'?>
-# <painting>
-#   <img src="madonna.jpg" alt='Foligno Madonna, by Raphael'/>
-#   <caption>This is Raphael's "Foligno" Madonna, painted in
-#   <date>1511</date>-<date>1512</date>.</caption>
-# </painting>
-#EOF
-
-
-
-
-
 
 
 
