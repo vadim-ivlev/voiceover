935da5cfdc19c9ced1bb410d8e06f79aefe9b6b4
Vadim Ivlev <vadimi.vlev@gmail.com>
2024-11-27 22:30:15 +0000

    rus voice sam.

--- sh/voiceover-rus.sh
+++ sh/voiceover-rus.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-go run ./cmd/voiceover -s 90 -e 100 -voices nova -translateto Russian -o texts/rus texts/dahl.txt
+go run ./cmd/voiceover -s 90 -e 100 -ttsapi elevenlabs -voices ulNeoiyl3bUW7oQjWZE8 -translateto Russian -o texts/rus texts/dahl.txt
 
