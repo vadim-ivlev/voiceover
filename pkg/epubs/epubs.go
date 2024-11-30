75f843675b245edb925e79ccbe79c2bed00f4490
Vadim Ivlev <vadimi.vlev@gmail.com>
2024-11-30 02:00:42 +0000

    read epub.

--- pkg/epubs/epubs.go
+++ pkg/epubs/epubs.go
@@ -1,28 +1,87 @@
 package epubs
 
 import (
-	"fmt"
-	"log"
-
-	// "github.com/bmaupin/go-epub"
-	"github.com/medialibraryonline/epub-go"
+	"archive/zip"
+	"io"
+	"path/filepath"
+	"sort"
+	"strings"
 )
 
-func main() {
-	// Open the EPUB
-	epubPath := "texts/dahl.epub"
-	book, err := epub.Open(epubPath)
+// ListEpubFiles returns the content of the EPUB file treating it as a zip file
+func ListEpubFiles(epubPath string) ([]string, error) {
+	r, err := zip.OpenReader(epubPath)
 	if err != nil {
-		log.Fatalf("Failed to open EPUB: %v", err)
+		return nil, err
 	}
+	defer r.Close()
 
-	// Get the title
-	title := book.Opf.Spine
-	fmt.Printf("Title: %s\n", title)
-
-	// Get the content
-	chapters := book.Files()
-	for i, chapter := range chapters {
-		fmt.Printf("Chapter %d:\n%s\n", i+1, chapter)
+	var files []string
+	for _, f := range r.File {
+		files = append(files, f.Name)
 	}
+	sort.Strings(files)
+	return files, nil
+}
+
+// filterExtension returns a slice of strings containing only the files with the specified extension
+func filterExtension(files []string, ext string) []string {
+	var filtered []string
+	for _, f := range files {
+		if filepath.Ext(f) == ext {
+			filtered = append(filtered, f)
+		}
+	}
+	return filtered
+}
+
+// listTranslatableFiles returns a slice of strings containing only the files that can be translated
+func listTranslatableFiles(files []string) []string {
+	translatableFiles := []string{}
+
+	ncxs := filterExtension(files, ".ncx")
+	sort.Strings(ncxs)
+	translatableFiles = append(translatableFiles, ncxs...)
+
+	xhtmls := filterExtension(files, ".xhtml")
+	sort.Strings(xhtmls)
+	translatableFiles = append(translatableFiles, xhtmls...)
+
+	htmls := filterExtension(files, ".html")
+	sort.Strings(htmls)
+	translatableFiles = append(translatableFiles, htmls...)
+
+	return translatableFiles
+}
+
+// getFileContent returns the content of the file with the specified name from the EPUB file
+func getFileContent(epubPath string, fileName string) (string, error) {
+	r, err := zip.OpenReader(epubPath)
+	if err != nil {
+		return "", err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		if f.Name == fileName {
+			rc, err := f.Open()
+			if err != nil {
+				return "", err
+			}
+			defer rc.Close()
+
+			return readAll(rc)
+		}
+	}
+	return "", nil
+}
+
+// readAll reads all the content from the reader and returns it as a string
+func readAll(r io.Reader) (string, error) {
+	buf := new(strings.Builder)
+	_, err := io.Copy(buf, r)
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
 }
--- pkg/epubs/epubs_test.go
+++ pkg/epubs/epubs_test.go
@@ -2,36 +2,46 @@ package epubs
 
 import (
 	"fmt"
-	"os"
 	"testing"
 
-	"github.com/vadim-ivlev/voiceover/internal/app"
-	"github.com/vadim-ivlev/voiceover/pkg/logger"
+	"github.com/vadim-ivlev/voiceover/pkg/utils"
 )
 
-func TestMain(m *testing.M) {
-	// No colors
-	logger.NoColor = true
-
-	// change directory to the root of the project
-	os.Chdir("../..")
-	app.InitLoggerSetParams()
-	fmt.Println("TestMain")
-
-	os.Exit(m.Run())
-}
-
-func Test_main(t *testing.T) {
+func TestListEpubFiles(t *testing.T) {
+	type args struct {
+		epubPath string
+	}
 	tests := []struct {
-		name string
+		name    string
+		args    args
+		wantErr bool
 	}{
+
 		{
-			name: "Test main",
+			name: "ListEpubFiles",
+			args: args{
+				epubPath: "texts/dahl.epub",
+			},
+			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			main()
+			got, err := ListEpubFiles(tt.args.epubPath)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ListEpubFiles() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			translatableFiles := listTranslatableFiles(got)
+			fmt.Printf("translatableFiles = \n%s\n", utils.PrettyJSON(translatableFiles))
+
+			ncxContent, err := getFileContent(tt.args.epubPath, translatableFiles[0])
+			if err != nil {
+				t.Errorf("getFileContent() error = %v", err)
+				return
+			}
+			fmt.Printf("ncxContent = \n%s\n", ncxContent)
+
 		})
 	}
 }
